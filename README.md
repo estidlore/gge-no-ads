5917313f185f89b9fb0c27fc80e9a2007cadd2db
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-10-05 14:10:10 -0500

    feat: setup extension
    - Add readme, manifest & icons
    - Add repository url & email
    - Add styles to hide side ads & ad-block disable pop-up
    - Wait for the ban from my fav game

---
+++ README.md
@@ -0,0 +1,4 @@
+GGE No Ads (GNA)
+
+Hide the GGE side ads and the requirement to disable the ad blocker
+Enjoy the game as it was before the annoying ads
---
+++ scripts/gge.js
@@ -0,0 +1,8 @@
+console.log(`
+----------------
+GGE No Ads (GNA)
+----------------
+https://github.com/estidlore/gge-no-ads
+estidlore@outlook.com
+----------------
+`);
